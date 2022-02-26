ba54f3ae8bf1b0be6cc99fcfc7e1ebb1d483af77
fpoveda <fpoveda@me.com>
2022-02-26 03:06:42 +0100

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,14 @@
+package paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Este es el main");
+		int a = 1;
+		int b = 2;
+		
+	}
+
+}
